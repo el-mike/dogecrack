f62ab0d9f54c549840ae6314393be8a1382a9fdb
el-mike <michal.huras5@gmail.com>
2021-12-16 23:27:04 +0100

    feat: go ssh client setup

---
+++ shepherd/go.mod
@@ -0,0 +1,8 @@
+module github.com/el-mike/dogecrack/shepherd
+
+go 1.17
+
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+)
--- shepherd/main.go
+++ shepherd/main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/knownhosts"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+		return
+	}
+
+	sshPassword := os.Getenv("SSH_PASSWORD")
+	sshUser := os.Getenv("SSH_USER")
+
+	hostKeyCb, err := knownhosts.New("~/.ssh/known_hosts")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	sshHost := "172.20.0.2:22"
+
+	sshConfig := &ssh.ClientConfig{
+		User:            sshUser,
+		HostKeyCallback: hostKeyCb,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(sshPassword),
+		},
+	}
+
+	conn, err := ssh.Dial("tcp", sshHost, sshConfig)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	defer conn.Close()
+}
