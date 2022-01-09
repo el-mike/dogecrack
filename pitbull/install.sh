4d272ddb8d7e9e639c37ae493e7277f1ac8d1dfd
el-mike <michal.huras5@gmail.com>
2022-01-10 00:16:25 +0100

    fix: tmux install

--- pitbull/install.sh
+++ pitbull/install.sh
@@ -4,6 +4,7 @@ apt -y install locales
 apt -y install wget
 apt -y install python3 python3-pip nano mc git python3-bsddb3
 apt -y install libssl-dev build-essential automake pkg-config libtool libffi-dev libgmp-dev libyaml-cpp-dev libsecp256k1-dev
+apt -y install tmux
 git clone https://github.com/3rdIteration/btcrecover.git
 pip3 install gdown
 pip3 install pyopencl==2019.1.1
