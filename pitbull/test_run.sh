0668e46c34f14d954d26a8aeb698756ef2b81d59
el-mike <michal.huras5@gmail.com>
2022-01-10 21:51:21 +0100

    chore: removing unnecessary script

--- pitbull/test_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tmux new-session -d -s "pitbull" "./pitbull.sh -f https://drive.google.com/uc?id=12ULZTz8X5tIZ4243DI_-pHwe65PgQ33E -w YmM6Hsef7JkyiFNcrxGH8pexipcyzuqAnM0KSpDjOGofzq2C4OkUDnNWNuVsAwAIg+l3"
